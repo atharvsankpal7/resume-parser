b3e0f27b16de78fa7610da844217d97cfa90f4bb
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2025-05-02 18:10:04 +0530

    removed ts type error

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,10 +6,9 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env');
 }
 
-let cached = global.mongoose;
-
+let cached: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null } = (global as any).mongoose;
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
